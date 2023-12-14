feb834efaf4d8b5e203e69106044c52b1615b46f
Vui Lam <vui@vmware.com>
2023-12-14 09:07:23 -0800

    add build-crd-package.sh

---
+++ hack/scripts/build-crd-package.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Copyright 2022 VMware, Inc. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set -e
+set -x
+
+
+BASE_DIR=$(cd $(dirname "${BASH_SOURCE[0]}"); pwd)
+PACKAGE_DIR=${BASE_DIR}/../../package
+BINDIR=${BASE_DIR}/../tools/bin
+
+#YTT?=${BINDIR}/ytt
+#KBLD?=${BINDIR}/kbld
+KCTRL=${KCTRL:-$BINDIR/kctrl}
+
+pushd ${PACKAGE_DIR}
+
+echo "XXXXXXXXXX $CRD_PACKAGE_IMAGE"
+
+yq e -i ".spec.template.spec.export[0].imgpkgBundle.image=\"${CRD_PACKAGE_IMAGE}\"" ./package-build.yml
+
+PATH=${BINDIR}:$PATH ${KCTRL} package release --yes
+
+popd
+
